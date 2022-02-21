b00beb05ea011012e47dbcd427213491bc7714d6
Nebojsa Markovic <nebojsa91markovic@gmail.com>
2022-02-21 22:10:11 +0100

    added PrivateRoute component with login logout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { useState } from "react";
+import PrivateRoute from "./components/PrivateRoute";
+import Profile from "./pages/Profile";
+import Account from "./pages/Account";
 
 function App() {
+  const [isAuth, setIsAuth] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Route path="/" exact>
+          <button onClick={() => setIsAuth(true)}>Login</button>
+          <button onClick={() => setIsAuth(false)}>Logout</button>
+          <br />
+          <Link to="/profile">Profile</Link>
+          <br />
+          <Link to="/account">Account</Link>
+        </Route>
+        <PrivateRoute path="/profile" component={Profile} isAuth={isAuth} />
+        <PrivateRoute path="/account" component={Account} isAuth={isAuth} />
+      </Router>
     </div>
   );
 }
